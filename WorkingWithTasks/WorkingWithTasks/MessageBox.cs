9ff4dc84760f9648c773c957f2e0529f2ff3fe20
Herman <germanriv19@gmail.com>
2022-10-25 21:42:41 +0300

    Done.

---
+++ WorkingWithTasks/WorkingWithTasks/MessageBox.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice
+{
+    public class MessageBox
+    {
+        public event Action<State> WindowHandler;
+
+        public async void Open()
+        {
+            Console.WriteLine("Window is open");
+            await Task.Delay(3000);
+            Console.WriteLine("Window was open by the user");
+            if (Random.Shared.Next(1, 3) == 1)
+            {
+                WindowHandler.Invoke(State.Ok);
+                return;
+            }
+
+            WindowHandler.Invoke(State.Cancel);
+        }
+    }
+}
---
+++ WorkingWithTasks/WorkingWithTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using Practice;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var task = new TaskCompletionSource();
+        var message = new MessageBox();
+        message.WindowHandler += PrintToConsole;
+        message.WindowHandler += x => task.SetResult();
+        message.Open();
+        await task.Task;
+    }
+
+    public static void PrintToConsole(State state)
+    {
+        if (state == State.Ok)
+        {
+            Console.WriteLine("OK - Operation was confirmed");
+            return;
+        }
+
+        Console.WriteLine("Cancel - Operation was canceled");
+    }
+}
